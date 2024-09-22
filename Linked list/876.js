19e7838f273a3a428edf97a45db35e6086ad9d38
emondewan00 <dewanmohammademon@gmail.com>
2024-09-22 22:58:00 +0600

    876 number problem solution added

--- Linked list/876.js
+++ Linked list/876.js
@@ -1,8 +1,14 @@
 // 876. Middle of the Linked List
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
-}
+
+  return slow;
+};
+
