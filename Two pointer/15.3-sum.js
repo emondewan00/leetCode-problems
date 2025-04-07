d0fffd85669e5de5f23c6cdea1c9c535b29a3367
emondewan00 <dewanmohammademon@gmail.com>
2025-04-07 22:50:36 +0600

    15 number leetcode problem solution added

---
+++ Two pointer/15.3-sum.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=15 lang=javascript
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b); 
+  let result = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue; // Skip duplicates
+
+    let left = i + 1,
+      right = nums.length - 1; // Two pointers
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+
+        // Skip duplicate values on the left
+        while (left < right && nums[left] === nums[left - 1]) left++;
+        // Skip duplicate values on the right
+        while (left < right && nums[right] === nums[right + 1]) right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return result;
+};
+
+const nums = [-1, 0, 1, 2, -1, -4];
+
+console.log(threeSum(nums));
+
+// @lc code=end
